e1e69b73a49923d494e7129945f09cac8a6e9436
周诚 <zhoucheng133@outlook.com>
2024-08-14 23:10:50 +0800

    添加acgrip的解析

--- utils/acgrip.js
+++ utils/acgrip.js
@@ -1,3 +1,24 @@
-export default function acgrip(){
-  
+import axios from "axios";
+import xml2js  from "xml2js"
+
+export default async function acgrip(){
+  const xml=(await axios.get('http://127.0.0.1:3000')).data;
+  const parser = new xml2js.Parser();
+  try {
+    parser.parseString(xml, (err, result) => {
+      if (err) {
+        // 解析错误
+        return;
+      }
+      var items=result.rss.channel[0].item;
+      for(let item of items){
+        console.log(item['title'][0].trim());
+        console.log(item['enclosure'][0]['$']["url"])
+      }
+    });
+  } catch (error) {
+    // 出现错误
+  }
 }
+
+acgrip();
