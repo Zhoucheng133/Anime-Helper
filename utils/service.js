9398b097058805178d6ab486e938411d13069bbf
周诚 <zhoucheng133@outlook.com>
2024-08-15 19:47:35 +0800

    添加服务指向

--- utils/service.js
+++ utils/service.js
@@ -1,6 +1,11 @@
-import state from "./variables.js"
+import mikan from "./mikan.js";
+import acgrip from "./acgrip.js";
+import state, {prels, ls} from "./data.js";
 
 export default function service(){
-  console.log(state.get());
-  
+  if(state.get().type=='mikan'){
+    mikan();
+  }else if(state.get().type=='acgrip'){
+    acgrip();
+  }
 }
