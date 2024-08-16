e77532f55186ef3ef1fa5689c87db4a05194bebb
周诚 <zhoucheng133@outlook.com>
2024-08-16 19:14:45 +0800

    添加data变量

--- web/src/states/form.ts
+++ web/src/states/form.ts
@@ -3,7 +3,15 @@ import { ref } from "vue";
 
 export default defineStore("form", ()=>{
   let running=ref(false);
+  let data=ref({
+    "type": "mikan",
+    "exclusions": [],
+    "bangumi": [],
+    "freq": 15,
+    "ariaLink": "",
+    "ariaSecret": ""
+  });
 
 
-  return { running };
+  return { running, data };
 })
--- web/src/view/form.vue
+++ web/src/view/form.vue
@@ -2,6 +2,18 @@
   <div class="form">
     <div class="item">
       <div class="form_label">运行状态</div>
+      <div class="form_content">
+        <a-tag :color="form().running ? 'success' : 'warning'" style="margin-right: 20px;">{{ form().running ? '运行中' : "等待中" }}</a-tag>
+        <a-switch v-model:checked="form().running" />
+        <a-button type="link" style="margin-left: 10px;">查看日志</a-button>
+      </div>
+    </div>
+    <div class="item">
+      <div class="form_label">RSS 来源</div>
+      <a-radio-group v-model:value="form().data.type" button-style="solid">
+        <a-radio-button value="mikan">Mikan</a-radio-button>
+        <a-radio-button value="acgrip">Acgrip</a-radio-button>
+      </a-radio-group>
     </div>
   </div>
 </template>
