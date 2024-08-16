ae4bf4f7e6d2ccc6ea5b8789e498ac0180857817
周诚 <zhoucheng133@outlook.com>
2024-08-16 18:53:51 +0800

    添加antd依赖

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import "./style.css";
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 const pinia = createPinia();
 
-createApp(App).use(pinia).mount('#app');
+createApp(App).use(Antd).use(pinia).mount('#app');
