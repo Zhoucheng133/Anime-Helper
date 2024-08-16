9347880e25e1558b2d97ebb0688d1670eaec2135
周诚 <zhoucheng133@outlook.com>
2024-08-16 19:48:12 +0800

    设置语言

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <div :style="{'width': width>800 ? '800px': '100vw'}" class="bg">
-    <Form/>
-  </div>
+  <a-config-provider :locale="locale">
+    <div :style="{'width': width>800 ? '800px': '100vw'}" class="bg">
+      <Form></Form>
+    </div>
+  </a-config-provider>
 </template>
 
 <script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
 import { ref } from 'vue';
 import Form from './view/form.vue';
-
+const locale=zhCN;
 let width=ref(window.innerWidth);
 window.onresize=()=>{
   width.value=window.innerWidth;
