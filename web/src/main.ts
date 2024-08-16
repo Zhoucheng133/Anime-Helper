6de20ac6c3ba9230b99359ac10447c855539be34
周诚 <zhoucheng133@outlook.com>
2024-08-16 19:19:13 +0800

    导入bootstrap图标

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue';
 import "./style.css";
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const pinia = createPinia();
 
