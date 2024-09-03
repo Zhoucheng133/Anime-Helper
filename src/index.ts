35586d63adcaf2687c5b23ee3670ad44c6414081
周诚 <zhoucheng133@outlook.com>
2024-09-03 23:18:25 +0800

    完成用户相关操作

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { Elysia } from "elysia"
 import { cors } from "@elysiajs/cors"
-import { jwt } from "@elysiajs/jwt"
+import jwt from "@elysiajs/jwt"
+import {Account} from "./router/account";
+import { response } from "./interface/interface";
+
+const account=new Account();
 
 const app = new Elysia()
 .use(cors())
-.use(jwt({ secret: process.env.JWT_SECRET as string }))
+.use(
+  jwt({name: 'jwt',secret: 'AnimeHelper', exp: "1y"})
+)
+.group("api", (app)=>
+  app
+  .post("/login", async ({ jwt, body }): Promise<response>=>await account.login(jwt, body))
+  .post("/register", ({ body })=>account.register(body))
+  .get("/check", async (): Promise<boolean>=>await account.checkInit())
+  .get("/auth", ({ jwt, headers})=>account.auth(jwt, headers))
+)
+
+// 静态的页面
 .get("*", () => Bun.file("web/index.html"))
 .get("/assets/:path", ({params: { path }}) => Bun.file("web/assets/"+path))
 .get("/icon.svg", () => Bun.file("web/icon.svg"))
