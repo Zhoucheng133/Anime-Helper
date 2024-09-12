f38b36d221876e362eb1f24b6dd03486b7a23f9e
周诚 <zhoucheng133@outlook.com>
2024-09-12 19:17:45 +0800

    更换到bangumi官方api

--- src/service/bangumi_service.ts
+++ src/service/bangumi_service.ts
@@ -1,29 +1,22 @@
 import axios from "axios";
-import * as cheerio from 'cheerio';
 
 export class BangumiService{
   async getList(): Promise<string[][]>{
     let ls=[] as string[][];
     try {
-      const response=await axios.get("https://bgm.tv/calendar");
-      const $ = cheerio.load(response.data);
-      $('li.week').each((index, weekElement) => {
-        let perLs=[] as string[];
-        $(weekElement).find('li').each((_, subLi) => {
-          const firstNav = $(subLi).find('.nav').first();
-          if (firstNav.length > 0) {
-            if(firstNav.html()?.length!=0){
-              perLs.push(firstNav.html()!)
-            }else{
-              const originalTitle = $(subLi).find('em').first();
-              if(originalTitle.html()?.length!=0){
-                perLs.push(originalTitle.html()!)
-              }
-            }
-          }
-        });
-        ls.push(perLs);
-      });
+      const response=(await axios.get("https://api.bgm.tv/calendar")).data;
+      for(var i=0; i<response.length; i++){
+        let innerls=[];
+        for(var j=0; j<response[i].items.length; j++){
+          innerls.push(response[i].items[j].name_cn.length==0?response[i].items[j].name:response[i].items[j].name_cn);
+        }
+        ls.push(innerls);
+      }
+      const ls6=ls.pop();
+      if(ls6!=undefined){
+        ls.unshift(ls6);
+      }
+
     } catch (_) {
       ls=[];
     }
