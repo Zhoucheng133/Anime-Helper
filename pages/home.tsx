1186acf556122a05cb361b16ff4b20168ddfd44c
zhoucheng <zhoucheng133@outlook.com>
2024-10-17 20:13:13 +0800

    添加初始化检查token

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,5 +1,11 @@
+import { initPage } from "@/hooks/init";
+
 export default function Home(){
   return <div>
     Home
   </div>
+}
+
+export async function getServerSideProps(context: any){
+  return initPage(context);
 }
