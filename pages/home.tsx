b6a73bd0f81e4897a0b8bf352daa5cd37b0a02a5
zhoucheng <zhoucheng133@outlook.com>
2024-10-17 20:14:12 +0800

    添加主页标题

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,11 @@
 import { initPage } from "@/hooks/init";
+import Head from "next/head";
 
 export default function Home(){
   return <div>
+    <Head>
+      <title>AnimeHelper | 主页</title>
+    </Head>
     Home
   </div>
 }
