4db66cff3dbda05c1fac789f3cc56905715666d6
zhoucheng <zhoucheng133@outlook.com>
2024-10-18 18:08:35 +0800

    添加注销功能

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,28 @@
-export default function header(){
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Cookies from 'js-cookie';
+
+interface HeaderProps {
+  login?: boolean;
+}
+
+export default function Header({login=false}: HeaderProps){
+
+  const logout=()=>{
+    Cookies.remove('token');
+    window.location.href='/login';
+  }
+
   return <div className="head_container">
     <div className="head_lead">
       <img src="/icon.svg" alt="" width="40px" className="head_icon"></img>
       <div className="head_title">AnimeHelper</div>
+    </div>
+    <div>
+      {login && (<div className="logoutArea" onClick={()=>logout()}>
+        <FontAwesomeIcon icon={faRightFromBracket} style={{'width': 15}}/>
+        <div className="logouLabel">注销</div>
+      </div>)}
     </div>
   </div>
 }
