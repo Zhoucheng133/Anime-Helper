fe014a82bfce4bbd0b27767d6f786010b802537a
zhoucheng <zhoucheng133@outlook.com>
2024-11-28 19:13:12 +0800

    修复一个异步问题

--- app/api/service/all.ts
+++ app/api/service/all.ts
@@ -13,35 +13,37 @@ export class All{
     const response=(await axios.get("http://127.0.0.1:5000")).data;
     const parser = new xml2js.Parser();
     try {
-      parser.parseString(response, (err, result) => {
-        if (err) {
-          return {
-            ok: false,
-            msg: '解析rss失败'
+      const result = await new Promise<any>((resolve, reject) => {
+        parser.parseString(response, (err, result) => {
+          if (err) {
+            reject({
+              ok: false,
+              msg: '解析rss失败'
+            });
+          } else {
+            resolve(result);
           }
-        }
-        var list=[];
-        var items=result.rss.channel[0].item;
-        for(let item of items){
-          list.push({
-            'title': item['title'][0].trim(),
-            'url': item['enclosure'][0]['$']["url"],
-          })
-        }
-        return {
-          ok: true,
-          msg: list,
-        }
+        });
       });
+  
+      const list = [];
+      const items = result.rss.channel[0].item;
+      for (let item of items) {
+        list.push({
+          'title': item['title'][0].trim(),
+          'url': item['enclosure'][0]['$']["url"],
+        });
+      }
+  
+      return {
+        ok: true,
+        msg: list,
+      };  
     } catch (error) {
       return {
         ok: false,
         msg: '解析rss失败'
       }
-    }
-    return {
-      ok: false,
-      msg: '解析rss失败'
     }
   }
 }
