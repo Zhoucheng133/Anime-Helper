7e6a4541de4051761624aeceaf6eb1e5872c4832
zhoucheng <zhoucheng133@outlook.com>
2024-11-28 20:36:33 +0800

    添加分页器

--- components/all/all_content.tsx
+++ components/all/all_content.tsx
@@ -1,8 +1,8 @@
 import { allColumn, AllItem, allStore, download } from "@/hooks/all";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button, ButtonGroup, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip, useDisclosure } from "@nextui-org/react";
-import { useState } from "react";
+import { Button, ButtonGroup, Pagination, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip, useDisclosure } from "@nextui-org/react";
+import { useMemo, useState } from "react";
 import { useRecoilValue } from "recoil"
 import AllAdd from "./all_add";
 
@@ -13,19 +13,38 @@ export default function AllContent(){
   const [thisItem, setThisItem]=useState<AllItem>({title: '', url: ''});
   const {isOpen: openAdd, onOpen: onOpenAdd, onClose: onCloseAdd} = useDisclosure();
 
+  const [page, setPage]=useState(1);
+  const items=useMemo(()=>{
+    const start = (page - 1) * 25;
+    const end = start + 25;
+    return list.slice(start, end);
+  }, [page, list]);
+  
   const add=(data: AllItem)=>{
     setThisItem(data);
     onOpenAdd();
   }
 
   return <div className="page">
-    <Table aria-label="content">
+    <Table aria-label="content" bottomContent={
+      <div className="flex w-full justify-center">
+        <Pagination
+          isCompact
+          showControls
+          showShadow
+          color="secondary"
+          page={page}
+          total={5}
+          onChange={(page) => setPage(page)}
+        />
+      </div>
+    }>
       <TableHeader columns={allColumn}>
         {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
       </TableHeader>
-      <TableBody items={list as AllItem[]}>
+      <TableBody items={items as AllItem[]}>
         {
-          list.map((item, index)=><TableRow key={index}>
+          items.map((item, index)=><TableRow key={index}>
             <TableCell>
               <Tooltip content={item.title}>
                 <div className="title_cell">
