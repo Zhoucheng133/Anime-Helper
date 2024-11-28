223208b7049e2f5aa17aba412baf18f0ab9924a5
zhoucheng <zhoucheng133@outlook.com>
2024-11-28 18:43:58 +0800

    修改header菜单名称

--- components/header.tsx
+++ components/header.tsx
@@ -49,7 +49,7 @@ export default function Header({login=false, page='list'}: HeaderProps){
       </div>
       <div className={ page=='all' ? "head_menu_item_selected" : 'head_menu_item'} style={{marginLeft: 30}} onClick={()=>toPage('all')}>
         <FontAwesomeIcon icon={faDownload} style={{'width': 12}}/>
-        <div className="head_menu_label">下载器</div>
+        <div className="head_menu_label">所有</div>
       </div>
       <div className={ page=='downloader' ? "head_menu_item_selected" : 'head_menu_item'} style={{marginLeft: 30}} onClick={()=>toPage('downloader')}>
         <FontAwesomeIcon icon={faDownload} style={{'width': 12}}/>
@@ -77,7 +77,7 @@ export default function Header({login=false, page='list'}: HeaderProps){
           <DropdownMenu aria-label="menus">
             <DropdownItem key="new" onClick={()=>toPage('list')}>列表</DropdownItem>
             <DropdownItem key="copy" onClick={()=>toPage('calendar')}>每日放送</DropdownItem>
-            <DropdownItem key="edit" onClick={()=>toPage('all')}>下载器</DropdownItem>
+            <DropdownItem key="edit" onClick={()=>toPage('all')}>所有</DropdownItem>
             <DropdownItem key="edit" onClick={()=>toPage('downloader')}>下载器</DropdownItem>
           </DropdownMenu>
         </Dropdown>
