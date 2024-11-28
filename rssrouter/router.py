d007fc99ea0a73e1330b84dc7bca5b1371db568f
zhoucheng <zhoucheng133@outlook.com>
2024-11-28 18:51:19 +0800

    添加一个测试路由

---
+++ rssrouter/router.py
@@ -0,0 +1,9 @@
+from flask import Flask, send_file
+app = Flask(__name__)
+
+@app.route('/')
+def rss():
+    return send_file('classic.xml', mimetype='application/xml')
+
+if __name__ == '__main__':
+    app.run(debug=False)
