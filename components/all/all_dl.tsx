6afa89179ef9d616aa404fddeda2e506f3676edb
zhoucheng <zhoucheng133@outlook.com>
2024-11-29 20:37:25 +0800

    完成下载的操作

--- components/all/all_dl.tsx
+++ components/all/all_dl.tsx
@@ -1,6 +1,43 @@
 import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
+import { useState } from "react";
+import Dialog from "../dialog";
+import axios from "axios";
+import { AllItem } from "@/hooks/all";
+import Cookies from 'js-cookie';
 
-export default function AllDl({isOpen, onClose, item}: any){
+interface prop{
+  isOpen: any,
+  onClose: any,
+  item: AllItem
+}
+
+export default function AllDl({isOpen, onClose, item}: prop){
+
+  const [openDialog, setOpenDialog]=useState(false);
+  const [msg, setMsg]=useState('');
+
+  const dlHandler=async ()=>{
+    const token=Cookies.get('token')
+    if(!token){
+      setOpenDialog(true);
+      setMsg('token不合法')
+      return;
+    }
+    const {data: res}=await axios.post('/api/all/download', {
+      'link': item.url
+    }, {
+      headers: {
+        token,
+      }
+    })
+    if(!res.ok){
+      setOpenDialog(true);
+      setMsg(res.msg);
+    }
+
+    onClose();
+  }
+
   return <>
     <Modal size="sm" isOpen={isOpen} onClose={onClose}>
       <ModalContent>
@@ -17,11 +54,12 @@ export default function AllDl({isOpen, onClose, item}: any){
           <Button onPress={onClose}>
             取消
           </Button>
-          <Button color="primary" onPress={onClose}>
+          <Button color="primary" onPress={()=>dlHandler()}>
             下载
           </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
+    <Dialog title="下载失败" msg={msg} isOpen={openDialog} onClose={()=>setOpenDialog(false)}></Dialog>
   </>
 }
