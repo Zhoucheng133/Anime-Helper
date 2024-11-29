9173bfda6d05726be987fd469faf630f2084b2ba
zhoucheng <zhoucheng133@outlook.com>
2024-11-29 20:04:13 +0800

    完成添加modal的layout

--- components/all/all_add.tsx
+++ components/all/all_add.tsx
@@ -1,5 +1,6 @@
 import { AllItem } from "@/hooks/all";
-import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
+import { Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
 interface props{
   item: AllItem,
@@ -9,15 +10,35 @@ interface props{
 
 export default function AllAdd({item, isOpen, onClose}: props){
 
+
+
+  useEffect(()=>{
+    const index1=item.title.indexOf('[');
+    const index2=item.title.indexOf(']');
+    if(index1!=-1 && index2!=-1){
+      setAss(item.title.slice(index1+1, index2));
+    }else{
+      setAss('');
+    }
+
+  }, [item])
+
   const add=()=>{
 
   }
 
+  const [title, setTitle]=useState('');
+  const [ass, setAss]=useState('');
+
   return <Modal size="md" isOpen={isOpen} onClose={onClose}>
     <ModalContent>
-      <ModalHeader className="flex flex-col gap-1">添加到</ModalHeader>
+      <ModalHeader className="flex flex-col gap-1">添加到下载器</ModalHeader>
       <ModalBody>
-        {item.title}
+        <div className="add_title">
+          {item.title}
+        </div>
+        <Input label='标题' value={title} onValueChange={setTitle}></Input>
+        <Input label='字幕组' value={ass} onValueChange={setAss}></Input>
       </ModalBody>
       <ModalFooter>
         <Button variant="light" onPress={onClose}>
