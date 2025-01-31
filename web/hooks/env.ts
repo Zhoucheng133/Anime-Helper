17208c513bdd3d7e283a6b38b627fcd47a690ca6
zhoucheng <zhoucheng133@outlook.com>
2025-01-31 18:30:51 +0800

    使用生产环境

--- web/hooks/env.ts
+++ web/hooks/env.ts
@@ -1,5 +1,5 @@
 // ssr的host地址，默认应该为http://127.0.0.1:8080
 export const ssrhost='http://127.0.0.1:8080';
 // 网页请求的host地址，开发模式下和ssr host相同，生产模式下应该为空字符串
-export const webHost='http://127.0.0.1:8080';
+export const webHost='';
 // export const webHost='';
