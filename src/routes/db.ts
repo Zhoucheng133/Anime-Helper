f7b376b078368fe68f9d1a2548ff982c60904e01
zhoucheng <zhoucheng133@outlook.com>
2025-03-10 19:41:22 +0800

    添加一些下载器相关表

--- src/routes/db.ts
+++ src/routes/db.ts
@@ -3,6 +3,9 @@ import Database from "bun:sqlite";
 export function initDB(db: Database){
   initUserTable(db);
   initListTable(db);
+  initDownloaderConfigTable(db);
+  initDownloaderListTable(db);
+  initDownloaderExcludeTable(db);
 }
 
 function initUserTable(db: Database){
@@ -23,6 +26,36 @@ function initListTable(db: Database){
       episode INTEGER,
       now INTEGER,
       time INTEGER
+    )
+  `).run()
+}
+
+function initDownloaderConfigTable(db: Database){
+  db.prepare(`
+    CREATE TABLE IF NOT EXISTS downloader_config (
+      link TEXT,
+      secret TEXT,
+      freq INTEGER,
+      type TEXT
+    )
+  `).run()
+}
+
+function initDownloaderListTable(db: Database){
+  db.prepare(`
+    CREATE TABLE IF NOT EXISTS downloader_list (
+      id TEXT KEY,
+      title TEXT,
+      ass TEXT
+    )
+  `).run()
+}
+
+function initDownloaderExcludeTable(db: Database){
+  db.prepare(`
+    CREATE TABLE IF NOT EXISTS downloader_exclude (
+      id TEXT KEY,
+      value TEXT
     )
   `).run()
 }
