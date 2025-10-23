725ecd02a49c13695da0c2496eaa65cb75e484ee
zhoucheng <zhoucheng133@outlook.com>
2025-10-23 22:13:52 +0800

    修复一个可能的token错误

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,10 +4,14 @@ export default async function auth(headers: any, jwt: any): Promise<ResponseType
   if (!headers || !headers.token) {
     return ToResponse(false, "参数不正确");
   }
-  const profile = await jwt.verify(headers.token);
-  if (profile.username) {
-    return ToResponse(true, "")
-  } else {
-    return ToResponse(false, "无效令牌")
+  try {
+    const profile = await jwt.verify(headers.token);
+    if (profile.username) {
+      return ToResponse(true, "")
+    } else {
+      return ToResponse(false, "无效令牌")
+    }
+  } catch (error: any) {
+    return ToResponse(false, "无效令牌");
   }
 }
