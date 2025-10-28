aaf98060c4334be40af3565bf70c00b9b5994b78
zhoucheng <zhoucheng133@outlook.com>
2025-10-28 16:37:03 +0800

    添加获取磁力链接的参数

--- src/routes/all.ts
+++ src/routes/all.ts
@@ -1,17 +1,16 @@
 import axios from "axios";
-import auth from "./auth";
 import { ResponseType, ToResponse } from "./types";
 import xml2js  from "xml2js";
 import Database from "bun:sqlite";
 import dayjs from "dayjs";
 import { DownloaderConfigType, downloadItem } from "./downloader";
-// import { all_get } from "./test";
 
 interface AllItem{
   time: number,
   title: string,
   url: string,
   length: number,
+  magnet: string
 }
 
 export class All{
@@ -36,26 +35,6 @@ export class All{
     }else{
       return ToResponse(false, "下载失败");
     }
-
-    // try {
-    //   await axios.post(
-    //     aria,
-    //     {
-    //       "jsonrpc": "2.0",
-    //       "method": "aria2.addUri",
-    //       "id": 1,
-    //       "params": [
-    //         `token:${secret}`,
-    //         [url],
-    //         {}
-    //       ],
-    //     }
-    //   );
-    // } catch (error) {
-    //   return ToResponse(false, error);
-    // }
-
-    // return ToResponse(true, "");
   }
 
   async get(): Promise<ResponseType>{
@@ -82,7 +61,8 @@ export class All{
           title: item['title'][0].trim(),
           url: item['enclosure'][0]['$']["url"],
           time: dayjs(item['torrent'][0]['pubDate'][0]).valueOf(),
-          length: item['torrent'][0]['contentLength'][0]
+          length: item['torrent'][0]['contentLength'][0],
+          magnet: `magnet:?xt=urn:btih:${item['torrent'][0]['link'][0].split('/').pop()}`,
         });
       }
       return ToResponse(true, list);
