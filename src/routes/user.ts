9b08d0a97890387d401d5b926aef9e415f382e6a
zhoucheng <zhoucheng133@outlook.com>
2026-05-11 01:16:05 +0800

    修复一个cookie有效期的问题

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -81,7 +81,7 @@ export class User{
 
     cookie.refresh_token.set({
       value: refreshToken,
-      maxAge: 30 * 24 * 60 * 60 * 1000,
+      maxAge: 30 * 24 * 60 * 60,
       httpOnly: true,
       path: "/api/refresh",
     })
