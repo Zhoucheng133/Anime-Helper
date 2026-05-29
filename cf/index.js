1b076459b8a562609ce6b139f241ad735bd04056
zhoucheng <zhoucheng133@outlook.com>
2026-05-29 18:01:10 +0800

    添加cf缓存设置

--- cf/index.js
+++ cf/index.js
@@ -2,21 +2,34 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': '*'
+        }
+      });
+    }
+
     if (url.pathname.includes('/pic/')) {
       url.hostname = 'lain.bgm.tv';
     } else {
       url.hostname = 'api.bgm.tv';
     }
     
+    const isGet = request.method === 'GET' || request.method === 'HEAD';
+    const cacheConfig = isGet ? { cacheEverything: true, cacheTtl: 3600 } : undefined;
+
     const newRequest = new Request(url, {
       method: request.method,
       headers: request.headers,
-      body: request.body,
+      body: isGet ? null : request.body,
       redirect: 'follow'
     });
 
     try {
-      const response = await fetch(newRequest);
+      const response = await fetch(newRequest, { cf: cacheConfig });
       const newResponse = new Response(response.body, response);
       newResponse.headers.set('Access-Control-Allow-Origin', '*');
       newResponse.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
